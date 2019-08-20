98ebd2c6339ecc3eb94c100a97a5fc269f772689
carlos.br.carb <carlos.br.carb@gmail.com>
2019-08-19 23:58:15 -0500

    toggle actions inside table

--- src/components/commons/CommonSelectInput.js
+++ src/components/commons/CommonSelectInput.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 class CommonSelectInput extends Component{
 
   render(){
-    const {idSection, isFormCircular, listItems, label, onChange, classContainer, classInput, isRequired} = this.props
+    const {idSection, isFormCircular, listItems, label, onChange, classContainer, classInput, isRequired, value} = this.props
     return(
       <div className={`form-group section-form ${(classContainer) ? classContainer : ''}`}>
         <label className={'label-form '+(isFormCircular ? 'circular':'')} htmlFor={idSection}>{label}</label>
         <select id={idSection+'select'} className={`form-control ${classInput}`}
-                onChange={(e)=> onChange(idSection, e.target.value)} required={isRequired}>
-          <option selected value={'0'}>Seleccione</option>
+                onChange={(e)=> onChange(idSection, e.target.value)} required={isRequired} defaultValue={value}>
+          <option selected value={''}>Seleccione</option>
           {
             listItems && listItems.length>0 ?
               listItems.map((item, index)=>{

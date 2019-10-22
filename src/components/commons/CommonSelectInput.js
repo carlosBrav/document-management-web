4b4216b210b955093877b4ad3320b0c01a76b52c
carlos.br.carb <carlos.br.carb@gmail.com>
2019-10-22 18:22:51 -0500

    integration API

--- src/components/commons/CommonSelectInput.js
+++ src/components/commons/CommonSelectInput.js
@@ -4,7 +4,6 @@ class CommonSelectInput extends Component{
 
   render(){
     const {idSection, isFormCircular, listItems, label, onChange, classContainer, classInput, isRequired, value} = this.props
-    console.log('LIST ITEMS ', listItems)
     return(
       <div className={`form-group section-form ${(classContainer) ? classContainer : ''}`}>
         <label className={'label-form '+(isFormCircular ? 'circular':'')} htmlFor={idSection}>{label}</label>
